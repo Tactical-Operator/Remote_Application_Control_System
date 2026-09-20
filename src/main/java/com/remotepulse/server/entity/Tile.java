7fd4296f85e389e013b3a1ccc8cb00899d1d93cc
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-09-20 21:58:18 +0530

    Tiles.java file uploaded

---
+++ src/main/java/com/remotepulse/server/entity/Tile.java
@@ -0,0 +1,98 @@
+package com.remotepulse.server.entity;
+
+import jakarta.persistence.*;;
+
+
+@Entity 
+@Table(name ="tiles")
+public class Tile {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private Integer slotNumber;
+
+    private String name;
+
+    private String type;
+
+    @Column(columnDefinition = "TEXT")
+    private String target;
+
+    private String icon;
+
+    public Tile(){
+
+    }
+
+    public Tile(Integer slotNumber){
+        this.slotNumber = slotNumber;
+
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public Integer getSlotNumber(){
+        return slotNumber;
+    }
+
+    public void setSlotNumber(Integer slotNumber){
+        this.slotNumber = slotNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getTarget(){
+        return target;
+    }
+
+    public void setTarget(String target){
+        this.target = target;
+    }
+
+    public String getIcon(){
+        return icon;
+    }
+
+    public void setIcon(String icon){
+        this.icon = icon;
+    }  
+}
+
+// What we're representing
+
+// A database record will eventually look like:
+
+// id:          1
+// slotNumber:  1
+// name:        Notepad
+// type:        APPLICATION
+// target:      C:\Windows\System32\notepad.exe
+// icon:        notepad.png
+
+// But an empty tile will be something like:
+
+// id:          1
+// slotNumber:  1
+// name:        null
+// type:        null
+// target:      null
+// icon:        null
