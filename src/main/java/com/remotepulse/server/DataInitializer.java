9b988a480bd3d922b8e5155e5ee652f23054c24e
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-09-21 12:57:28 +0530

    DataInitializer.java added

---
+++ src/main/java/com/remotepulse/server/DataInitializer.java
@@ -0,0 +1,30 @@
+package com.remotepulse.server;
+
+import com.remotepulse.server.entity.Tile;
+import com.remotepulse.server.repository.TileRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component 
+public class DataInitializer implements CommandLineRunner{
+
+    private final TileRepository tileRepository;
+
+    public DataInitializer(TileRepository tileRepository){
+        this.tileRepository = tileRepository;
+    }
+
+    @Override 
+    public void run(String...args){
+
+        for(int i = 1; i <= 8;i++){
+
+            if(tileRepository.findBySlotNumber(i).isEmpty()){
+
+                Tile tile = new Tile();
+                tileRepository.save(tile);
+            }
+        }
+    }
+    
+}
