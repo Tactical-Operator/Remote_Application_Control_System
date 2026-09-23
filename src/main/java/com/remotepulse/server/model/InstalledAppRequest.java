9825d01598186939db8be797a5af630a1e9d6274
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-09-23 16:03:41 +0530

    Structure for entering value for Installed App

---
+++ src/main/java/com/remotepulse/server/model/InstalledAppRequest.java
@@ -0,0 +1,35 @@
+package com.remotepulse.server.model;
+
+public class InstalledAppRequest {
+
+    private Integer slotNumber;
+    private String name;
+    private String appId;
+
+    public InstalledAppRequest() {
+    }
+
+    public Integer getSlotNumber() {
+        return slotNumber;
+    }
+
+    public void setSlotNumber(Integer slotNumber) {
+        this.slotNumber = slotNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+}
