01af6182c75cee4926d72d7bec8067e7d3fe52af
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-09-23 21:18:33 +0530

    Added blueprint for cutom app details

---
+++ src/main/java/com/remotepulse/server/model/CustomAppRequest.java
@@ -0,0 +1,35 @@
+package com.remotepulse.server.model;
+
+public class CustomAppRequest{
+
+    private Integer slotNumber;
+    private String name;
+    private String path;
+
+    public CustomAppRequest() {
+    }
+
+    public Integer getSlotNumber() {
+        return slotNumber;
+    }
+
+    public void setSlotNumber(Integer slotNumber) {
+        this.slotNumber = slotNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
