5f9ba4470b349107429e9341ad2fe67211b75ef5
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-09-24 21:08:10 +0530

    added website data blueprint

---
+++ src/main/java/com/remotepulse/server/model/WebsiteRequest.java
@@ -0,0 +1,35 @@
+package com.remotepulse.server.model;
+
+public class WebsiteRequest{
+
+    private Integer slotNumber;
+    private String name;
+    private String url;
+
+    public WebsiteRequest() {
+    }
+
+    public Integer getSlotNumber() {
+        return slotNumber;
+    }
+
+    public void setSlotNumber(Integer slotNumber) {
+        this.slotNumber = slotNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
