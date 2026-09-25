29e0adba02c9746b889cdcb8a8c099d8c2036ef4
ASHWIN JADHAV <ashwinj2110@gmail.com>
2026-09-25 21:59:16 +0530

    minor updates

--- src/main/java/com/remotepulse/server/service/InstalledAppSaveService.java
+++ src/main/java/com/remotepulse/server/service/InstalledAppSaveService.java
@@ -1,16 +1,29 @@
+
 package com.remotepulse.server.service;
 
 import com.remotepulse.server.entity.Tile;
 import com.remotepulse.server.repository.TileRepository;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Path;
+
 @Service
 public class InstalledAppSaveService {
 
     private final TileRepository tileRepository;
 
-    public InstalledAppSaveService(TileRepository tileRepository) {
-        this.tileRepository = tileRepository;
+    private final IconExtractionService iconExtractionService;
+
+    public InstalledAppSaveService(
+            TileRepository tileRepository,
+            IconExtractionService iconExtractionService
+    ) {
+
+        this.tileRepository =
+                tileRepository;
+
+        this.iconExtractionService =
+                iconExtractionService;
     }
 
     public Tile saveInstalledApp(
@@ -19,18 +32,60 @@ public class InstalledAppSaveService {
             String appId
     ) {
 
-        Tile tile = tileRepository
-                .findBySlotNumber(slotNumber)
-                .orElseThrow(() ->
-                        new RuntimeException(
-                                "Tile slot not found: " + slotNumber
-                        )
-                );
+        Tile tile =
+                tileRepository
+                        .findBySlotNumber(slotNumber)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "Tile slot not found: " +
+                                        slotNumber
+                                )
+                        );
 
         tile.setName(name);
+
         tile.setType("INSTALLED_APP");
+
         tile.setTarget(appId);
+
+        try {
+
+            String outputPath =
+                    Path.of(
+                            "src",
+                            "main",
+                            "resources",
+                            "static",
+                            "icons",
+                            "slot-" +
+                                    slotNumber +
+                                    ".png"
+                    )
+                    .toAbsolutePath()
+                    .toString();
+
+            iconExtractionService.extractInstalledAppIcon(
+                    appId,
+                    outputPath
+            );
+
+            tile.setIcon(
+                    "slot-" +
+                            slotNumber +
+                            ".png"
+            );
+
+        } catch (Exception e) {
+
+            System.out.println(
+                    "Could not extract icon for: " +
+                    name
+            );
+
+            e.printStackTrace();
+        }
 
         return tileRepository.save(tile);
     }
 }
+
